c79c9fc7a4701f670ffed0b7e87d40e1971ca884
pipelinedave <pipelinedave@users.noreply.github.com>
2023-04-26 10:37:16 +0000

    jekyll build from Action 4171b1651e397f8a3390e82f09c190e2e5ec720f

---
+++ fetch-github-posts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone Gists
+git clone git://gist.github.com/1234567.git _posts/gists/1234567
+git clone git://gist.github.com/2345678.git _posts/gists/2345678
+
+# Clone Repositories
+git clone git://github.com/user/repo.git _posts/repos/repo
+git clone git://github.com/user/other-repo.git _posts/repos/other-repo
